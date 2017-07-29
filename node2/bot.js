68292a6fa77a49906ef404c43ca34aa07218a5b1
sevelee <seve.lee@hotmail.com>
2017-07-29 12:05:57 +0800

    输出每个搜索到的内容

--- node2/bot.js
+++ node2/bot.js
@@ -7,12 +7,17 @@ var config = require('../../config')
 var client = new Twitter(config)
 
 var params = {
-    q: '中古',
+    q: 'ガンダム',
     count: 2
 }
 
 client.get('search/tweets', params, gotData)
 
 function gotData(err, data, response) {
-    console.log(data)
+    var tweets = data.statuses
+    var length = tweets.length
+    while (length--) {
+        console.log(tweets[length].text)
+        console.log('------------------------')
+    }
 }
