194bf2e0ba15b1fcda02f055e499e53701296ae6
sevelee <seve.lee@hotmail.com>
2017-07-29 11:27:49 +0800

    问题： twitter API 无法通过shadowsocks翻墙，twit API没有提供proxy的选项，待解决

--- node2/bot.js
+++ node2/bot.js
@@ -4,4 +4,16 @@ var twit = require('twit')
 
 var config = require('../../config')
 
-var T = new twit(config)
+var myTwit = new twit(config)
+
+var params = {
+    q: 'rainbow',
+    count: 2
+}
+
+console.log('start to twit get')
+myTwit.get('search/tweets', params, GotData)
+
+function GotData(err, data, response) {
+    console.log(data)
+}
