1fdc323a46bd151adcd3a2c83adf05eaa0aa5be9
sevelee <seve.lee@foxmail.com>
2017-07-31 21:58:54 +0800

    利用stream功能添加了follow event

--- node2/bot2.js
+++ node2/bot2.js
@@ -6,16 +6,43 @@ var config = require('../../config')
 
 var client = new Twitter(config)
 
-var tweet = {
-    status: '#codingrainbow from node.js'
+// config over
+
+//----------------follow反应
+
+var stream = client.stream('user')
+
+stream.on('follow', Followed)
+
+function Followed(eventMsg) {
+    console.log(' - follow event - ')
+    var name = eventMsg.source.name
+    var screenName = eventMsg.source.screen_name
+    TweetIt('. @' + screenName + ' Hello and thanks for following !~')
 }
 
-client.post('statuses/update', tweet, tweeted)
+//----------------random tweet
+// TweetIt('new message test')
 
-function tweeted(err, data, response) {
-    if (err) {
-        console.log('We get a error')
-    } else {
-        console.log(data)
+// 函数名， 时间， 函数参数
+// setInterval(TweetIt, 1000 * 20, 'new message')
+
+function TweetIt(msg) {
+
+    var r = Math.floor(Math.random()*100)
+
+    var tweet = {
+        // status: 'random nuber ' + r + ' ' + msg
+        status: msg
+    }
+
+    client.post('statuses/update', tweet, tweeted)
+
+    function tweeted(err, data, response) {
+        if (err) {
+            console.log('We get a error')
+        } else {
+            console.log('Sent finished')
+        }
     }
 }
