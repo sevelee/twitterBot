ef482824686fdaa62208d4bc33431f52cbbda936
sevelee <seve.lee@foxmail.com>
2017-07-31 23:58:15 +0800

    能够成功读取特定城市的pm

--- node2/pm.js
+++ node2/pm.js
@@ -4,8 +4,17 @@ var pm_token = pm_config.token
 
 var city = 'beijing'
 
-var pm_path = 'https://api.waqi.info' + '/feed/:' + city + '/?token=:' + pm_token
+var pm_path = 'https://api.waqi.info' + '/feed/' + city + '/?token=' + pm_token
 
-app.get(pm_path, function(err, data) {
-    console.log(data)
+var getJSON = require('get-json')
+
+console.log(pm_path)
+
+getJSON(pm_path, function(error, response) {
+    if (error) {
+        console.log('get pm data error.')
+    } else {
+        console.log(response.data.city.name)
+        console.log(response.data.iaqi.pm25.v)
+    }
 })
