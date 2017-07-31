1d2dc1558003031ab62920b2d08b773fa59123d9
sevelee <seve.lee@foxmail.com>
2017-08-01 01:42:02 +0800

    成功的可以产生图片然后上传了

--- node2/bot2.js
+++ node2/bot2.js
@@ -4,34 +4,44 @@ var Twitter = require('twitter')
 
 var config = require('../../config')
 
+var powerShell = require('powershell')
+
+var fs = require('fs')
+
 var client = new Twitter(config)
 
 // config over
 
+//---------------main
+
+TweetIt('#coderainbow from node.js')
+
 //----------------processing 触发
 
-var processingCmd = 'processing-java --sketch=$PWD\\processingDraw --run'
+function MakeNewImage() {
+    console.log('make image start.')
+    var processingCmd = 'processing-java --sketch=$PWD\\processingDraw --run'
 
-var powerShell = require('powershell')
+    let ps = new powerShell(processingCmd)
 
-let ps = new powerShell(processingCmd)
-
-ps.on('output', data => {
-    console.log('processing cmd over.')
-})
+    ps.on('output', data => {
+        console.log('make image over.')
+        return true
+    })
+}
 
 //----------------follow反应
 
-var stream = client.stream('user')
+// var stream = client.stream('user')
 
-stream.on('follow', Followed)
+// stream.on('follow', Followed)
 
-function Followed(eventMsg) {
-    console.log(' - follow event - ')
-    var name = eventMsg.source.name
-    var screenName = eventMsg.source.screen_name
-    TweetIt('. @' + screenName + ' Hello and thanks for following !~')
-}
+// function Followed(eventMsg) {
+//     console.log(' - follow event - ')
+//     var name = eventMsg.source.name
+//     var screenName = eventMsg.source.screen_name
+//     TweetIt('. @' + screenName + ' Hello and thanks for following !~')
+// }
 
 //----------------random tweet
 // TweetIt('new message test')
@@ -40,19 +50,34 @@ function Followed(eventMsg) {
 // setInterval(TweetIt, 1000 * 20, 'new message')
 
 function TweetIt(msg) {
+    MakeNewImage()
 
-    var r = Math.floor(Math.random()*100)
-
-    var tweet = {
-        // status: 'random nuber ' + r + ' ' + msg
-        status: msg
+    console.log('upload image start.')
+    var fileName = 'processingDraw/output.png'
+    var params = {
+        encoding: 'base64'
     }
+    var b64content = fs.readFileSync(fileName, params)
 
-    client.post('statuses/update', tweet, tweeted)
+    client.post('media/upload', {media_data: b64content}, Uploaded)
+
+    function Uploaded(err, data, response) {
+        console.log('image uploaded.')
+        var imageID = data.media_id_string
+        if (msg == '') {
+            msg = '#coderainbow from node.js'
+        }
+        var tweet_params = {
+            status: msg + ' ' + Math.floor(Math.random() * 100),
+            media_ids: imageID
+        }
+        client.post('statuses/update', tweet_params, tweeted)
+    }
 
     function tweeted(err, data, response) {
         if (err) {
             console.log('We get a error')
+            console.log(err)
         } else {
             console.log('Sent finished')
         }
